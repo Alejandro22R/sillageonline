db2ccfba1bf8033d843e1ac5aeb22fff3669bd89
Alejandro <alejandrorivera221200@gmail.com>
2026-06-02 19:25:20 -0400

    descripcion

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ $title ?? 'Sillage Parfums' }}</title>
+        <meta name="description" content="Descubre fragancias de lujo en Sillage Parfums. Los mejores perfumes para hombre y mujer con envíos a todo el país.">
         
         <link rel="icon" type="image/png" href="{{ asset('img/sillage.png') }}">
         
